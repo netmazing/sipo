39ccc01e9478bd2c787722567016136163cd61b5
Dev <p.trojnacki@o2.pl>
2019-09-23 15:39:34 +0200

    create navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,34 @@
-import React from 'react'
+import React, {useState} from 'react';
+import {Link} from "gatsby";
+import "../css/navbar.css";
+import { FaBars } from 'react-icons/fa';
+import links from '../constants/links';
 
 const Navbar = () => {
+  const [isOpen, setNav] = useState(false);
+  const toggleNav = () => {
+    setNav(isOpen => !isOpen);
+  }
     return (
-        <div>
-          <h2>Navbar</h2>  
-        </div>
+        <nav className="navbar">
+          <div className="menu">
+            <div className="menu__header">
+              <div className="menu__logo">Logo</div>
+              <button type="button" className="menu__button" onClick={toggleNav}>
+                <FaBars className="menu__icon"/>
+              </button>
+            </div>
+
+            <ul className={isOpen ? "menu__links menu__links--open" : "menu__links"}>
+              {links.map((link, index) => {
+                return(
+                  <li className="menu__item" key={index}><Link className="menu__link" to={link.path}>{link.name}</Link></li>
+                )
+              })}
+            </ul>
+
+          </div> 
+        </nav>
     )
 }
 
---
+++ src/constants/links.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        path: '/',
+        name: 'start',
+    },
+    {
+        path: '/zespol',
+        name: 'zespół',
+    },
+    {
+        path: '/publikacje',
+        name: 'publikacje',
+    },
+    {
+        path: '/wydarzenia',
+        name: 'wydarzenia',
+    },
+    {
+        path: '/kontakt',
+        name: 'kontakt',
+    },
+]
